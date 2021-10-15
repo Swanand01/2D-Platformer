145d7e410240ba0ddfea0b23ba28cdf0ac63be18
Swanand01 <swanandmathekar@gmail.com>
2021-10-15 17:54:14 +0530

    Moving Platforms added

---
+++ Assets/Scripts/WaypointFollower.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointFollower : MonoBehaviour
+{
+    [SerializeField] private GameObject[] waypoints;
+    private int currentWaypointIndex = 0;
+
+    [SerializeField] private float speed = 2f;
+    private int factor = 0;
+
+    private void Update()
+    {
+        if (Vector2.Distance(waypoints[currentWaypointIndex].transform.position, transform.position) < 0.1f)
+        {
+            if (currentWaypointIndex == 0)
+            {
+                factor = 1;
+            }
+            if (currentWaypointIndex == waypoints.Length - 1)
+            {
+                factor = -1;
+            }
+
+            currentWaypointIndex += factor;
+
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, waypoints[currentWaypointIndex].transform.position, Time.deltaTime * speed);
+    }
+}
